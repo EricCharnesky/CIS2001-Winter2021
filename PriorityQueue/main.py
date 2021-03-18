0393fe7cbbc9315f47f83e1ddea900b564a31f09
Eric Charnesky <eric.charnesky@careevolution.com>
2021-03-18 16:29:07 -0400

    priority queue complete

--- PriorityQueue/main.py
+++ PriorityQueue/main.py
@@ -1,4 +1,7 @@
-class PriorityQueue:
+import random
+
+
+class MinimumPriorityQueue:
 
     # use binary tree structure in array based storage
 
@@ -16,17 +19,90 @@ class PriorityQueue:
         self._data = [None]
         self._number_of_items = 0
 
+    # O(log n)
     def add(self, key, value=None):
+        if self._number_of_items == len(self._data):
+            self._increase_size()
+
+        item = self._Item(key, value)
+        self._data[self._number_of_items] = item
+        self._upheap(self._number_of_items)
+
         self._number_of_items += 1
 
-    def remove_min(self):
-        pass
+    def _increase_size(self):
+        new_data = [None] * len(self._data) * 2
+        for index in range(len(self._data)):
+            new_data[index] = self._data[index]
+        self._data = new_data
 
+    def _upheap(self, index):
+        # stops when index is the top of the heap
+        if index != 0:
+            parent_index = self._parent(index)
+
+            if self._data[index] < self._data[parent_index]:
+                self._swap(index, parent_index)
+                self._upheap(parent_index)
+
+    def _swap(self, index, new_index):
+        temp = self._data[index]
+        self._data[index] = self._data[new_index]
+        self._data[new_index] = temp
+
+        # one liner, because python is awesome
+        # self._data[index], self._data[new_index] = self._data[new_index], self._data[index]
+
+    def _downheap(self, index):
+        smallest_child_index = None
+        if self._is_valid_index(self._right_child(index)):
+            smallest_child_index = self._right_child(index)
+            if self._data[self._left_child(index)] < self._data[smallest_child_index]:
+                smallest_child_index = self._left_child(index)
+        elif self._is_valid_index(self._left_child(index)):
+            smallest_child_index = self._left_child(index)
+
+        if smallest_child_index is not None:
+            if self._data[smallest_child_index] < self._data[index]:
+                self._swap(smallest_child_index, index)
+                self._downheap(smallest_child_index)
+
+    # O(log n)
+    def remove_min(self):
+        self._number_of_items -= 1
+        self._swap(0, self._number_of_items)
+        item = self._data[self._number_of_items]
+        self._data[self._number_of_items] = None
+        self._downheap(0)
+        return item._key, item._value
+
+    def _parent(self, index):
+        return (index - 1) // 2
+
+    def _left_child(self, index):
+        return index * 2 + 1
+
+    def _right_child(self, index):
+        return index * 2 + 2
+
+    def _is_valid_index(self, index):
+        return index < self._number_of_items and index >= 0
+
+    # O(1)
     def min(self):
-        return self._data[0]
+        return self._data[0]._key, self._data[0]._value
 
     def __len__(self):
         return self._number_of_items
 
     def is_empty(self):
         return len(self) == 0
+
+
+priority_queue = MinimumPriorityQueue()
+
+for number in range(20):
+    priority_queue.add(random.randint(0,100))
+
+while not priority_queue.is_empty():
+    print(priority_queue.remove_min())
